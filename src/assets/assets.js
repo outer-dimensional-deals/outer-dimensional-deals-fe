901150368d2981a655d3fa71d02ef56b253746bc
Neil Linton <lintonbneil@gmail.com>
2023-01-10 21:18:24 -0600

    Add files and skeleton folders

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import { IoLogoPlaystation, IoLogoXbox, IoLogoWindows, SiNintendo } from '@chakra-ui/react'
+
+
+export const assets = [
+    {name: 'PlayStation', img: <IoLogoPlaystation/> },
+    {name: 'PC', img: <IoLogoWindows/> },
+    {name: 'Xbox', img: <IoLogoXbox/> },
+    {name: 'Nintendo', img: <SiNintendo />}
+]
