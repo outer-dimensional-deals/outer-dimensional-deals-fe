e69ce28973e8f63c623c60b49c8aa32c154c2f29
Neil Linton <lintonbneil@gmail.com>
2023-06-21 20:59:32 -0500

    Add files to render dumby data

---
+++ src/dumby-data/fightingData.js
@@ -0,0 +1,24 @@
+
+
+<Tab className='TEXT'>ADVENTURE</Tab>
+<Tab className='TEXT'>ARCADE</Tab>
+<Tab className='TEXT'>CARD AND BOARD</Tab>
+<Tab className='TEXT'>FIGHTING</Tab>
+<Tab className='TEXT'>HACK AND SLASH</Tab>
+<Tab className='TEXT'>INDIE</Tab>
+<Tab className='TEXT'>MOBA</Tab>
+<Tab className='TEXT'>MUSIC</Tab>
+<Tab className='TEXT'>PINBALL</Tab>
+<Tab className='TEXT'>PLATFORM</Tab>
+<Tab className='TEXT'>POINT AND CLICK</Tab>
+<Tab className='TEXT'>TRIVIA</Tab>
+<Tab className='TEXT'>RACING</Tab>
+<Tab className='TEXT'>RTS</Tab>
+<Tab className='TEXT'>RPG</Tab>
+<Tab className='TEXT'>SHOOTER</Tab>
+<Tab className='TEXT'>SIMULATION</Tab>
+<Tab className='TEXT'>SPORTS</Tab>
+<Tab className='TEXT'>STRATEGY</Tab>
+<Tab className='TEXT'>TACTICAL</Tab>
+<Tab className='TEXT'>TURN BASED</Tab>
+<Tab className='TEXT'>VISUAL NOVEL</Tab>
